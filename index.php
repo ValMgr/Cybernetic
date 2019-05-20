d1c429fa52d8976552c0e5e115c96d9b0ff73c6d
V.Mgr <ValMgr@users.noreply.github.com>
2019-05-20 23:37:42 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cybernetik</title>
+</head>
+<body>
+
+
+
+<h1>Voici le temps restant:</h1>
+
+<p id="date"></p>
+
+    
+</body>
+
+
+<script src="countdown.js"></script>
+
+</html>
