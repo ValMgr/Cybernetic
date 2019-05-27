18107a59fafe9147a5c315f62eb616cc8814a897
V.Mgr <ValMgr@users.noreply.github.com>
2019-05-27 16:43:33 +0200

    Try Hexagon

--- index.php
+++ index.php
@@ -6,7 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CyberNeTic</title>
     <link rel="stylesheet" type="text/css" media="screen" href="style/main.css" />
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" type="text/css" media="screen" href="style/hexagon.css" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous"> 
 
 </head>
 <body>
@@ -25,14 +27,33 @@
         </div>
 
     
-        <div class="content">
 
         
-                <h1>TEST</h1>
+                <div class="navbar">
+                    <a href="#">Actualités</a>
+                    <a href="#" id="select">Projet</a>
+                    <a href="#">Formulaire</a>
+                    <a href="#">Contact</a>
+                    <a href="#">ABCDAIR</a>
+                    <a href="#"><i class="fas fa-search"></i></a>
+                </div>
 
-                <p>Lorem Ipsum laof kafof kakfalfle flaff lf akfe lfakf </p>
-        
-        </div>
+                <div class="content">
+
+                    <h2>À propos du projet </h2>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                     In vulputate quam vitae tincidunt scelerisque. Aliquam ut tortor nec metus egestas rutrum.
+                      Nulla lobortis purus ipsum, sed porta enim vehicula quis. Cras nec diam massa. Nulla venenatis dignissim urna.
+                       In commodo tristique gravida. Phasellus a dignissim erat. Donec rhoncus finibus varius. Nulla in hendrerit sapien.
+                        Phasellus lorem est, mattis in vulputate et, aliquet vel erat. Quisque auctor dignissim sollicitudin.
+                         Proin ut nunc hendrerit magna finibus porta. Duis bibendum pretium mattis. </p>
+
+                </div>
+
+                <div class="HexagonGroup1">
+                    <div class="hexagon"></div>
+                    <div class="hexagon2"></div>
+                </div>
 
     </div>
 
