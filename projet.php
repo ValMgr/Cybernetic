1850420ad03b07445d4188b12bd58bee177c941a
Max <maximewarluzel@gmail.com>
2019-05-29 13:10:34 +0200

    rerepush page projet

---
+++ projet.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CyberNeTic</title>
+    <link rel="stylesheet" type="text/css" media="screen" href="style/main.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="style/mainResponsive.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="style/projet.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="style/projetResponsive.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="style/hexagon.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="style/hexagonResponsive.css" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous"> 
+
+</head>
+    <body>
+
+        <div class="wrapper">
+
+                <div class="navbar">
+                    <a href="#">ACTUALITES</a>
+                    <a id="select" href="#">PROJET</a>
+                    <a href="#">TEMOIGNAGE</a>
+                    <a href="#">EN IMAGE</a>
+                    <a href="#">RESSOURCES</a>
+                    <a href="#">CONTACT</a>
+                    <a href="#"><i class="fas fa-search"></i></a>
+                </div>
+
+                <div id="Maille1"></div>
+
+                <div class="HexagonGroup1">
+                    <div class="hexagone3">
+                        <div id="hexagon3-1"></div>
+                        <div id="hexagon3-2"></div>
+                        <div id="hexagon3-3"></div>
+                    </div>
+                    <div class="hexagone4">
+                        <div id="hexagon4-1"></div>
+                        <div id="hexagon4-2"></div>
+                        <div id="hexagon4-3"></div>
+                    </div>
+                    <h1 id="hexagon3-title">A propos du projet</h1>
+                </div>
+
+                <div id="desc">
+                    <div class="descrption"></div>
+                    <!-- <h2>CYBERNETIC C'EST QUOI ?</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                    <h2>OBJECTIFS DU PROJET</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+ -->                </div>
+
+        </div>
+
+    </body>
+</html>
