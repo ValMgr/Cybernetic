b8b063609ff2862f3066afb82105c7b738e8efc0
Max <maximewarluzel@gmail.com>
2019-06-20 17:27:24 +0200

    Creation Footer

--- Lang/FR/FOOTER.php
+++ Lang/FR/FOOTER.php
@@ -1,4 +1,17 @@
 <!-- FOOTER - Lang = FR -->
 
+    <div id="mainfooter">
+        <a href="#"><i class="fab fa-facebook-f fa-3x"></i></a>
+        <br><br>
+        <a href="#"><i class="fab fa-instagram fa-3x"></i></a>
+        <hr>
+        <h4>Mentions Légales</h4>
+        <h4>Flux RSS</h4>
+        <h4>Contact</h4>
+        <h4>Que'est-ce que Cybernetic ?</h4>
+    </div>
+    <div id="mainfooter2">
+        <h5>Ⓒ Copyright Cybernetic</h5>        
+    </div>
 
-                    <!-- END FOOTER -->
+<!-- END FOOTER -->
