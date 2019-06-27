5fdcd4c481f8d9172c6238bbc9e6f4b2d84895e7
Max <maximewarluzel@gmail.com>
2019-06-27 14:58:30 +0200

    fin refonte homepage et fix bugs

--- Lang/FR/index.php
+++ Lang/FR/index.php
@@ -27,27 +27,17 @@
 
 
                     <div id="particles-js" class="SAH" style="top: 10%;"></div>
-                
 
-
-                    <div class="About">
-
-                    
-
-                        <p> En partenariat avec la Gendarmerie Nationale, le projet CyberNeTic vise à mener une recherche-action au coeur des sciences
-                            de l'information et de la communication pour étudier les formes d'expression de la violence au moyen des nouvelles
-                            technologies et tout particulièrement celle du cyberharcèlement. Il s'agit d'analyser les discours d'expériences
-                            (victimes, enquêteurs) et les échanges de conversations (sms, réseaux sociaux, mails, etc.), afin d'identifier 
-                            les phénomènes d'engrenage, les stratégies d'influence et de manipulation enclenchées par les prédateurs.
-                        </p>
-
-                        <a href="projet.php" class="More">En savoir plus</a>
-
+                    <div class="HexagonGroupHome0">
+                        <div id="hexagonHome0"></div>
+                        <div id="hexagonHome0-0"></div>
+                        <h2 id="hexagonHome0-title">Accueil</h2>
                     </div>
 
-                    <div id="UselessBar"></div>
-                    
+                    <div id="AboutDiv"></div>
 
+                    <div id="hexagonHomeDeco1"></div>
+            
                     <div class="HexagonGroupHome1">
                         <div class="Projetglitch" >
                             <div class="hexagonHome1 glitch_"></div>
@@ -57,55 +47,63 @@
                             <div class="hexagonHome1 glitch_"></div>
                         </div>
 
-                        <!-- <div class="hexagonHome1"></div> -->
-                        <div id="hexagonHome1-1"></div>
                         <div id="hexagonHome2"></div>
-                        <div id="hexagonHome2-2"></div>
                         <h3 id="hexagonHome1-title">LE PROJET</h3>
                     </div>
 
+                    <div id="UselessBar1"></div>
 
-                    <div class="Actuality"> <h2>ACTUALITES</h2> </div>
+                    <div class="About">
+                        <p> 
+                            En partenariat avec la Gendarmerie Nationale, le projet CyberNeTic vise à mener une recherche-action au coeur des sciences de l'information et de la communication pour étudier les formes d'expression de la violence au moyen des nouvelles technologies et tout particulièrement celle du cyberharcèlement. Il s'agit d'analyser les discours d'expériences (victimes, enquêteurs) et les échanges de conversations (sms, réseaux sociaux, mails, etc.), afin d'identifier les phénomènes d'engrenage, les stratégies d'influence et de manipulation enclenchées par les prédateurs.
+                        </p>
+                    </div>
+
+                    <div id="UselessBar2"></div>
+
+                    <div id="Aboutlink">
+                        <a href="projet.php" class="More">En savoir plus</a>
+                    </div>
+
+
+
+                    <div class="Actuality"> <h2><i class="far fa-newspaper"></i> ACTUALITES</h2> </div>
 
                     <div class="LastActu">
                         <div id="LastActu1">
-                            <img src="style/Assets/Actu1.jpg" style="width: 100%;">
-                            <h3> Le corps du délit </h3>
-                            <p> De la séduction à l’humiliation, le cyberharcèlement en question, les instrumentalisations plurielles du corps dans le cadre de cyberviolences.</p>
-                            <a href="#"><i class="fas fa-globe-europe"></i> Lieu </a>
+                            <img src="style/Assets/Actu1.jpg" style="width: 100%; border-radius: 5px 5px 0px 0px;">
+                            <h3> Le corps du délit, de la séduction à l’humiliation</h3>
+                            <p> Le cyberharcèlement en question, les instrumentalisations plurielles du corps dans le cadre de cyberviolences.</p>
                             <p class="ActuDate"><i class="far fa-clock"></i> 03.06.19</p>
                         </div>
                         <div id="LastActu1-1"></div>
 
                         <div id="LastActu2">
-                            <img src="style/Assets/Actu2.jpg" style="width: 100%;">
-                            <h3 class="longtitle"> Dark net & cybercriminalité</h3>
+                            <img src="style/Assets/Actu2.jpg" style="width: 100%; border-radius: 5px 5px 0px 0px;">
+                            <h3> Dark net & cybercriminalité</h3>
                             <p>Le hacking au secours des organisations dans le cadre du programme de recherche CyberNeTic.</p>
-                            <a href="#"><i class="fas fa-globe-europe"></i> Lieu </a>
                             <p class="ActuDate"><i class="far fa-clock"></i> 03.06.19</p>
                         </div>
                         <div id="LastActu2-2"></div>
 
                         <div id="LastActu3">
-                            <img src="style/Assets/Actu3.jpg" style="width: 100%;">
-                            <h3 class="longtitle"> Stratégies amoureuses & cyberharcèlement </h3>
+                            <img src="style/Assets/Actu3.jpg" style="width: 100%; border-radius: 5px 5px 0px 0px;">
+                            <h3> Stratégies amoureuses & cyberharcèlement </h3>
                             <p>Quand le discours manipulateur devient un crime de langue sur les réseaux sociaux. <br><br></p>
-                            <a href="#"><i class="fas fa-globe-europe"></i> Lieu </a>
                             <p class="ActuDate"><i class="far fa-clock"></i> 03.06.19</p>
                         </div>
                         <div id="LastActu3-3"></div>
                     </div>
 
-                    <div class="ActuMore"><a href="Actualites.php" class="More">EN VOIR PLUS</a></div>
+                    <div id="ActuMore"><a href="Actualites.php" class="More">En savoir plus</a></div>
 
 
-                    <!-- <div id="MailleHome2"></div> -->
+                    <div id="hexagonHomeDeco2"></div>
 
-                    <div class="Partner">
-                        <h2>NOS PARTENAIRES</h2>
+
+                    <div id="Partners">
+                            <span><i class="far fa-handshake"></i> PARTENAIRES</span>
                     </div>
-
-                    <!-- <div id="MailleHome3"></div> -->
 
                     <div class="HexagonGroupHome2">
                         <img class="hexagonHome3" src="style/Assets/Logo-NTech.jpg" style="padding: 20px 0px;">
@@ -126,7 +124,9 @@
                     </div>
 
 
-                    <?php include 'FOOTER.php'; ?>
+                    <div id="hexagonHomeDeco3"></div>
 
+
+                    <?php include 'FOOTER.php'; ?>
 
     </div>
